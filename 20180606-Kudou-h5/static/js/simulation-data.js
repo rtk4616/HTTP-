f0b1bf253b52db0a08130aa9adf2a52145811d11
2416451355@qq.com <314314@wang>
2018-06-21 17:31:16 +0800

    sdasdfasdfas

---
+++ 20180606-Kudou-h5/static/js/simulation-data.js
@@ -0,0 +1,215 @@
+const goddessDrawInfo = {
+    "status": "0",
+    "remark": "成功",
+    "goddessDrawInfoList": [
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "12345678"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "4612386"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "74125845"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "36985214"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "7894569"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20170512"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "4565386"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "4568956"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "54365365"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "69887967"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "56746765"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "5674576"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "6785678"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "23425345"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "4563456"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "763567"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "465674567"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "568467846"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "97897897"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "578985679"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "987089"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "7890789"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "34563456"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "4563456"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "4563456"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "5764567"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "45375556"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "45634535"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20069781"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+        {
+            "encryNumber": "159****6545",
+            "ticketId": "20180011"
+        },
+
+    ]
+}
